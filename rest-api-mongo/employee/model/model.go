0f53ee67ce2bab3c728016fcaf98759e4d6fa81e
Keerthi Kumaran <KumaranKeerthi>
2019-02-21 20:40:08 -0500

    model changes -- removing main.go from project root

--- rest-api-mongo/employee/model/model.go
+++ rest-api-mongo/employee/model/model.go
@@ -1,1 +1,13 @@
 package model
+
+type Employee struct {
+	ID        string   `json:"id,omitempty"`
+	FirstName string   `json:"firstname,omitempty"`
+	LastName  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
